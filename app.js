5bbd4a4e445b00a5b4e3250e98a62a4f00d65c74
zhwang <516275397@qq.com>
2016-05-26 16:32:56 +0800

    完善路由接口和漂流瓶逻辑

--- app.js
+++ app.js
@@ -1,18 +1,94 @@
 var mongodb = require('./models/mongodb');
 var redis = require('./models/redis');
 var express = require('express');
-var app = express();
 
+var app = express();
 app.use(express.bodyParser());
 
+// 扔一个漂流瓶 或 扔回海里一个漂流瓶
+// POST owner=xxx&type=xxx&content=xxx[&time=xxx]
+// 当是“扔回”的时候，time 参数是必需的
+app.post('/', function (req, res) {
+    // 检查参数
+    if (!(req.body.owner && req.body.type && req.body.content)) {
+        return res.json({code: 0, msg: '信息不完整'});
+    }
+
+    // 检查类型
+    if (req.body.type && ['male', 'female'].indexOf(req.body.type) === -1) {
+        return res.json({code: 0, msg: '类型错误'});
+    }
+
+    //检查扔瓶子次数，扔一个瓶子
+    redis.throw(req.body, function (result) {
+        res.json(result);
+    });
+});
+
+// 捡一个漂流瓶
+// GET /?user=xxx[&type=xxx]
 app.get('/', function (req, res) {
-    res.json({
-        msg: "success"
+    // 检查user
+    if (!req.query.user) {
+        return res.json({code: 0, msg: '信息不完整'});
+    }
+
+    // 检查类型
+    if (req.query.type && (['male', 'female', 'all'].indexOf(req.query.type) === -1)) {
+        return res.json({code: 0, msg: '类型错误'});
+    }
+
+    // 捡漂流瓶
+    redis.pick(req.query, function (result) {
+        if (result.code === 1) {
+            mongodb.save(req.query.user, result.msg, function (err) {
+                if (err) {
+                    return res.json({code: 0, msg: '获取漂流瓶失败，请重试'});
+                }
+                return res.json(result);
+            });
+        }
+        res.json(result);
+    });
+});
+
+// 获取一个用户所有的漂流瓶
+// GET /user/nswbmw
+app.get('/user/:user', function (req, res) {
+    mongodb.getAll(req.params.user, function (result) {
+        res.json(result);
+    });
+});
+
+// 获取特定 id 的漂流瓶
+// GET /bottle/529a8b5b39242c82417b43c3
+app.get('/bottle/:_id', function (req, res) {
+    mongodb.getOne(req.params._id, function (result) {
+        res.json(result);
+    });
+});
+
+// 回复特定 id 的漂流瓶
+// POST user=xxx&content=xxx[&time=xxx]
+app.post('/reply/:_id', function (req, res) {
+    if (!(req.body.user && req.body.content)) {
+        return res.json({code: 0, msg: '回复信息不完整！'});
+    }
+    mongodb.reply(req.params._id, req.body, function (result) {
+        res.json(result);
+    });
+});
+
+// 删除特定 id 的漂流瓶
+// GET /delete/529a8b5b39242c82417b43c3
+app.get('/delete/:_id', function (req, res) {
+    mongodb.delete(req.params._id, function (result) {
+        res.json(result);
     });
 });
 
 app.listen(3000, function (err) {
-    if(err) {
+    if (err) {
         return console.log('Start server fail');
     }
 
